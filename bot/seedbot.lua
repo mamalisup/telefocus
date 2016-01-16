187ec23395605e3fa8e2b4f281e1d0138320adac
mamalisup <rezam7023@gmail.com>
2016-01-16 18:24:35 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {119650184,157187288,tonumber(our_id)},--Sudo users
+    sudo_users = {157187288}(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -230,19 +230,13 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@amirho3ein911 [Founder]
-@mamaligodem [Developer]
+@mamaligodem [Founder]
 
 
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+mamaligodem
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@telefocus [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
